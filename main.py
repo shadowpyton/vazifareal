7dcefb06e039744334dbe257889e724d10a5787f
shadowpyton <132641686+shadowpyton@users.noreply.github.com>
2023-05-24 11:26:24 +0500

    Azizbek

---
+++ main.py
@@ -0,0 +1,18 @@
+sinf = {}
+sinf["sinf_raqam"] = "7-b"
+sinf["sinf_rahbar"] = "Nilufar"
+sinf["o_soni"] = "20"
+sinf["fan_soni"] = "18"
+
+# print(sinf.items())
+# print(sinf.keys())
+# print(sinf.values())
+
+oquvchi = {
+    "ism":"Azizbek",
+    "yoshi":14,
+
+}
+
+lugatlar = [sinf,oquvchi]
+print(lugatlar[0]['o_soni'])
